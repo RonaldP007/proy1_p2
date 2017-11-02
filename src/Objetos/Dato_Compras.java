6a0341a1b5999d3d03cf5d2f9d069869c82708d1
Ronald <Ronald@DESKTOP-GQMAF0N>
2017-11-01 21:14:34 -0600

    Objeto Dato_Compras

---
+++ src/Objetos/Dato_Compras.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author Ronald
+ */
+public class Dato_Compras {
+    String nombre;
+    int cedula;
+    String correo;
+    String nombre_articulo;
+    int cantidad_comprado;
+    String tipo_articulo;
+    
+    public Dato_Compras(String nombre, int cedula, String correo, String nombre_articulo, int cantidad_comprado,String tipo_articulo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.nombre_articulo = nombre_articulo;
+        this.cantidad_comprado = cantidad_comprado;
+        this.tipo_articulo = tipo_articulo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula (int cedula) {
+        this.cedula  = cedula ;
+    }
+
+    public String getCorreo() {
+        return correo ;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo  = correo ;
+    }
+
+    public String getNombre_Articulo() {
+        return nombre_articulo;
+    }
+
+    public void setNombre_Articulo(String nombre_articulo) {
+        this.nombre_articulo= nombre_articulo;
+    }
+
+    public int getCantidad_Comprado() {
+        return cantidad_comprado;
+    }
+
+    public void setCantidad_Comprado(int cantidad_comprado) {
+        this.cantidad_comprado = cantidad_comprado;
+    }
+    public String getTipo_Articulo() {
+        return tipo_articulo;
+    }
+
+    public void setTipo_Articulo(String tipo_articulo) {
+        this.tipo_articulo = tipo_articulo;
+    }
+    
+}
